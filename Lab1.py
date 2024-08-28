827e57ae782ff9c682ffcad37cc31fe93c32ed57
OliviaB-19 <obeck02@saintmarys.edu>
2024-08-28 17:08:06 -0400

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,117 @@
+#authors'name are:
+
+print("Hello World!")
+print("Welcome to CPSC 207!")
+print("Hello\nWorld")
+print("How\nare\nyou")
+
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+"Hello World!"
+'Hello World!'
+'Hello World!'
+'Hello World!'
+"Hello Olivia"
+'Hello Olivia'
+2+2
+4
+4-2
+2
+4*2
+8
+4/2
+2.0
+5%2
+1
+5**2
+25
+5//2
+2
+-5//2
+-3
+3.14-0.14
+3.0
+print(2+2)
+4
+print("2+2")
+2+2
+print("Hello")
+Hello
+print("123")
+123
+print("123"+"45"+"6")
+123456
+print("123"+"n\45"+"n\6")
+123n%n
+
+============ RESTART: C:/Users/olivi/AppData/Local/Programs/Python/Python312/Lab1.py ============
+Welcome to CPSC 207!
+
+
+#authors'name are:
+print("Hello World!")
+Hello World!
+print("Welcome to CPSC 207!")
+Welcome to CPSC 207!
+print("Hello\nWorld")
+Hello
+World
+print("How\nare\nyou")
+How
+are
+you
+print("Today","is","Wednesday")
+Today is Wednesday
+print("Today"+"is"+"Wednesday")
+TodayisWednesday
+print("Hello\n"+"\nWorld!")
+Hello
+
+World!
+print("\n123","\n45","\n6")
+
+123 
+45 
+6
+print("\n123"+"\n45"+"\n6")
+
+123
+45
+6
+
+print("1+2*3")
+1+2*3
+1+2*3
+7
+(1+2)*3
+9
+7/3
+2.3333333333333335
+7//3
+2
+7%3
+1
+22//4
+5
+22%4
+2
+3**2
+9
+2**3
+8
+(4+3)//2
+3
+6%2
+0
+1+2*3**4
+163
+print("2+2")
+2+2
+print("2+2",2+2)
+2+2 4
+print("2+2=",2+2)
+2+2= 4
+print("50 pounds is", 50*0.453592,"Kilograms")
+50 pounds is 22.6796 Kilograms
+
+
